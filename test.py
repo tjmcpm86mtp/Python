bd18329777e578b07c4cfdea4daab2a362400332
tjmcpm86mtp <tjmcpm86mtp@outlook.jp>
2022-04-24 20:44:38 +0900

    初日の最終版。

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ capture = cv2.VideoCapture(0)
 
 while(True):
     ret, frame = capture.read()
-    cv2.imshow('frame',frame)
+    cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
